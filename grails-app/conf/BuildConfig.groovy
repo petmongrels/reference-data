c27af354dcb105ebaca2147e12bb91fbfc8204f4
Deepak N <endeep123@gmail.com>
2014-01-17 17:41:22 +0530

    #1515 | Removed migration plugin and update liquibase version

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -65,7 +65,6 @@ grails.project.dependency.resolution = {
 
         // plugins needed at runtime but not for compilation
         runtime ":hibernate:3.6.10.6" // or ":hibernate4:4.1.11.6"
-        runtime ":database-migration:1.3.8"
         runtime ":jquery:1.10.2.2"
         runtime ":resources:1.2.1"
 
