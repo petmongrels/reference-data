eabd6d893e16b2b53056e034604d851732a507cc
Deepak N <endeep123@gmail.com>
2014-01-22 14:01:59 +0530

    #1515 | Fixing drop db script

--- scripts/tearDownDB.sh
+++ scripts/tearDownDB.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-psql -Upostgres -c "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = 'clinlims';"
+psql -Upostgres -c "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = 'reference_data';"
 psql -Upostgres -c "drop database reference_data";
 psql -Upostgres -c "drop user reference_data";
