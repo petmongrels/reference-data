d36eae6143ac40ad78b55309ee9e6723a0e2eae9
Vinay Venu <vinayv@thoughtworks.com>
2014-01-29 12:26:05 +0530

    Vinay | Adapt to change in JdbcConnectionProvider interface

--- src/java/org/bahmni/referenceData/service/ReferenceDataConnectionProvider.java
+++ src/java/org/bahmni/referenceData/service/ReferenceDataConnectionProvider.java
@@ -1,6 +1,12 @@
 package org.bahmni.referenceData.service;
 
 import org.ict4h.atomfeed.jdbc.JdbcConnectionProvider;
+import sun.reflect.generics.reflectiveObjects.*;
+
+import java.lang.*;
+import java.lang.Override;
+import java.lang.RuntimeException;
+import java.lang.UnsupportedOperationException;
 import java.sql.SQLException;
 import java.sql.Connection;
 
@@ -18,5 +24,28 @@ public class ReferenceDataConnectionProvider implements JdbcConnectionProvider {
 
     @Override
     public void closeConnection(Connection connection) throws SQLException {
+    }
+
+    @Override
+    public void startTransaction() {
+        throw new UnsupportedOperationException("This was meant to be for a server, not client. ");
+    }
+
+    @Override
+    public void commit() {
+        try {
+            connection.commit();
+        } catch (SQLException exception) {
+            throw new RuntimeException(exception);
+        }
+    }
+
+    @Override
+    public void rollback() {
+        try {
+            connection.rollback();
+        } catch (SQLException exception) {
+            throw new RuntimeException(exception);
+        }
     }
 }
