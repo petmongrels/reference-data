6834911343dcd5ccfe3dc814cffa1ba9e1594f1b
Hemanth <hemanthfeb25@gmail.com>
2014-03-14 15:26:05 +0530

    Banka, Hemanth | #1904 | fixed UOM for a product changing from one category to another. made sales unit of measure should same as purchase unit of measure

--- grails-app/domain/org/bahmni/referenceData/domain/Drug.groovy
+++ grails-app/domain/org/bahmni/referenceData/domain/Drug.groovy
@@ -26,12 +26,26 @@ class Drug {
         salePrice(nullable: false)
         category(nullable: false)
         form(nullable: false)
-        saleUnitOfMeasure(nullable: false)
-        purchaseUnitOfMeasure(nullable: false)
+        saleUnitOfMeasure(nullable: false, validator: {val, obj -> Drug.validateUOMCategory(val, obj, 'saleUnitOfMeasure')})
+        purchaseUnitOfMeasure(nullable: false, validator: {val, obj -> Drug.validatePurchaseUOMCategory(val, obj, 'purchaseUnitOfMeasure')})
     }
 
     static mapping = {
         sort isActive : "desc"
+    }
+
+    static validateUOMCategory(newUnitOfMeasure, drug, field) {
+        ProductUnitOfMeasure oldUnitOfMeasure = drug.getPersistentValue(field)
+        if(oldUnitOfMeasure != null && !oldUnitOfMeasure.getCategory().getId().equals(newUnitOfMeasure.getCategory().getId())) {
+            return ["Drug.uom.validator.error", oldUnitOfMeasure.getCategory(), newUnitOfMeasure.getCategory()]
+        }
+    }
+
+    static validatePurchaseUOMCategory(newUnitOfMeasure, drug, field){
+        if (!drug.getSaleUnitOfMeasure().getCategory().getId().equals(drug.getPurchaseUnitOfMeasure().getCategory().getId())){
+            return ["Drug.uom.mismatch.category.error", drug.getSaleUnitOfMeasure().getCategory(), drug.getPurchaseUnitOfMeasure().getCategory()]
+        }
+        return validateUOMCategory(newUnitOfMeasure, drug, field)
     }
 
     static belongsTo = [category: DrugCategory, form: DrugForm, saleUnitOfMeasure: ProductUnitOfMeasure, purchaseUnitOfMeasure: ProductUnitOfMeasure]
