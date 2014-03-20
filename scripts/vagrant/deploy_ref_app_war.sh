a89fc84ac54d646b7c773b93c6512aea4d416030
Gurpreet Luthra <gluthra@thoughtworks.com>
2014-03-20 14:57:03 +0530

    Gurpreet, BalajiN | #1965 | Setting user to JSS for copied artifacts in vagrant

--- scripts/vagrant/deploy_ref_app_war.sh
+++ scripts/vagrant/deploy_ref_app_war.sh
@@ -4,4 +4,4 @@ TEMP_LOCATION=/tmp/deploy_ref_app
 REF_APP_WAR_LOCATION=/home/jss/apache-tomcat-7.0.22/webapps
 
 sudo rm -rf $REF_APP_WAR_LOCATION/reference-data
-sudo cp -f $TEMP_LOCATION/* $REF_APP_WAR_LOCATION
+sudo su - jss -c "cp -f $TEMP_LOCATION/* $REF_APP_WAR_LOCATION"
